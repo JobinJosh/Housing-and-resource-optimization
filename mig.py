682db7d3baed6a2d671c1a5f53140f5d61a1a5f2
JobinJosh <126863768+JobinJosh@users.noreply.github.com>
2025-02-26 21:28:22 +0100

    Update mig.py

--- mig.py
+++ mig.py
@@ -106,8 +106,8 @@ st.title('Economic Migrants')
 st.subheader('Venezuelan Migrants in Colombia')
 
 num_persons = st.number_input('Number of Persons', min_value=1, step=1, value=10000)
-income_min = st.number_input('Minimum Salary ($)', min_value=0, value=300)
-income_max = st.number_input('Maximum Salary ($)', min_value=0, value=650)
+income_min = st.number_input('Minimum Salary (US Dollar)', min_value=0, value=300)
+income_max = st.number_input('Maximum Salary (US Dollar)', min_value=0, value=650)
 
 education_probs = [
     st.slider('No Education Probability', 0.0, 1.0, 0.03),
